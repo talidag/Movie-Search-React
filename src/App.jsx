a13214d100440c4579f1885ae2a87e95f7f04a44
talidag <120104711+talidag@users.noreply.github.com>
2023-05-24 08:47:13 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import SearchMovies from './SearchMovies'
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1 className="title">React Movie Search</h1>
+        <SearchMovies />
+      </div>
+    </>
+  )
+}
+
+export default App
